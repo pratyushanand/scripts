c4a93318eadf9f10b490127f8164ad73afa5d9dc
Pratyush Anand <pratyush.anand@gmail.com>
2018-12-22 15:32:45 +0530

    Update setup to use vimrc and vim plugin
    Signed-off-by: Pratyush Anand <pratyush.anand@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ exportpathcmd="export PATH="$PATH:$(pwd)""
 cat ~/.bashrc | grep -q "PATH.*scripts" || echo $exportpathcmd >> ~/.bashrc
 source ~/.bashrc
 [[ -L ~/.gitconfig ]] || [[ -f ~/.gitconfig ]] || ln -s $(pwd)/gitconfig ~/.gitconfig
+[[ -L ~/.vimrc ]] || [[ -f ~/.vimrc ]] || ln -s $(pwd)/vimrc ~/.vimrc
+[[ -L ~/.vim ]] || [[ -d ~/.vim ]] || ln -s $(pwd)/vim ~/.vim
